f492811a35a098cce4a66ad7e810dd9c0dd146df
janani <chravija@gmail.com>
2025-03-18 20:01:56 +0530

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3500, // Set Vite's frontend server to run on port 3000
+    port: 3500, // Set frontend to run on port 3500
+    strictPort: true, // Ensures the exact port is used
+    hmr: {
+      overlay: false, // Disable WebSocket error overlay
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8000', // Your backend URL
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        ws: true, // Enable WebSocket proxy
       }
     }
   }
